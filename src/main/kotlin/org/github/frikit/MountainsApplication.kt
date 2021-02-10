7227fc7db9cc76a62ac9316146a9cc3b13b54e14
frikit <osipovvictor1994@gmail.com>
2021-02-10 21:04:06 +0000

    generate spring project

---
+++ src/main/kotlin/org/github/frikit/MountainsApplication.kt
@@ -0,0 +1,11 @@
+package org.github.frikit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MountainsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MountainsApplication>(*args)
+}
